34faf094b7ce87428e8e220434bf6c691d892cbd
Peeedroferreira <peeedroferreira1992@gmail.com>
2024-04-21 12:10:38 -0300

    Atualizaçãões das imagens e efeitos de tansição

--- js/displayImages.js
+++ js/displayImages.js
@@ -10,28 +10,42 @@ function displayImages() {
         imageContainer.innerHTML = '';
         imageContainer.style.display = 'block';
 
-        const images = ['imagem1.jpg', 'imagem2.jpg', 'imagem3.jpg',
-        'imagem4.jpg', 'imagem5.jpg'];
-        let currentImageIndex = 0;
+        const images = [
+            'fotos (1).jpg', 'fotos (2).jpg', 'fotos (3).jpg', 'fotos (4).jpg', 'fotos (5).jpg', 
+            'fotos (6).jpg', 'fotos (7).jpg', 'fotos (8).jpg', 'fotos (9).jpg', 'fotos (10).jpg', 
+            'fotos (11).jpg', 'fotos (12).jpg', 'fotos (13).jpg', 'fotos (14).jpg', 'fotos (15).jpg', 
+            'fotos (16).jpg', 'fotos (17).jpg', 'fotos (18).jpg', 'fotos (19).jpg', 'fotos (20).jpg', 
+            'fotos (21).jpg', 'fotos (22).jpg', 'fotos (23).jpg', 'fotos (24).jpg', 'fotos (25).jpg', 
+            'fotos (26).jpg', 'fotos (27).jpg', 'fotos (28).jpg', 'fotos (29).jpg', 'fotos (30).jpg', 
+            'fotos (31).jpg', 'fotos (32).jpg', 'fotos (33).jpg', 'fotos (34).jpg', 'fotos (35).jpg',
+            'fotos (36).jpg', 'fotos (37).jpg'
+        ];
+    
+        function getRandomImageIndex() {
+            return Math.floor(Math.random() * images.length);
+        }
+
+        let currentImageIndex = getRandomImageIndex();
 
         const img = document.createElement('img');
         img.src = `images/${images[currentImageIndex]}`;
         img.style.width = '100%';
         img.style.maxHeight = '500px';
         img.style.objectFit = 'contain';
+        img.style.transition = 'opacity 1s ease-in-out';
+        img.style.opacity = 0;
         imageContainer.appendChild(img);
 
-        function nextImage() {
-            currentImageIndex++;
-            if (currentImageIndex < images.length) {
-                img.src = `images/${images[currentImageIndex]}`;
-            } else {
-                currentImageIndex = 0;
-                img.src = `images/${images[currentImageIndex]}`;
-            }
-            
-        }
-        nextImage();
-        setInterval(nextImage, 5000);
+    function nextImage() {
+        img.style.opacity = 0;
+        setTimeout(() => {
+            currentImageIndex = getRandomImageIndex();
+            img.src = `images/${images[currentImageIndex]}`;
+            img.style.opacity = 1;
+        }, 1000);
+    }
+
+    nextImage();
+    setInterval(nextImage, 5000);
     }
 }
